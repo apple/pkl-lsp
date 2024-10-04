2870ccacfa0c6baa85bd6a71a7b35e24cba15686
Dan Chao <dan.chao@apple.com>
2024-10-04 10:47:33 -0700

    Introduce ParserSnippetTests
    This introduces parser snippet tests, which parses input text, and writes
    out the AST of the parse tree.
    
    This also moves some existing tests from `ParserTest` to `ParserSnippetTests`.

---
+++ src/test/kotlin/org/pkl/lsp/ParserSnippetTestEngine.kt
@@ -0,0 +1,298 @@
+/**
+ * Copyright © 2024 Apple Inc. and the Pkl project authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.pkl.lsp
+
+import java.nio.file.Path
+import java.util.Locale
+import kotlin.io.path.*
+import kotlin.reflect.KClass
+import org.assertj.core.api.Assertions.fail
+import org.assertj.core.util.diff.DiffUtils
+import org.junit.platform.engine.*
+import org.junit.platform.engine.TestDescriptor.Type
+import org.junit.platform.engine.discovery.ClassSelector
+import org.junit.platform.engine.discovery.MethodSelector
+import org.junit.platform.engine.discovery.PackageSelector
+import org.junit.platform.engine.discovery.UniqueIdSelector
+import org.junit.platform.engine.support.descriptor.*
+import org.junit.platform.engine.support.hierarchical.EngineExecutionContext
+import org.junit.platform.engine.support.hierarchical.HierarchicalTestEngine
+import org.junit.platform.engine.support.hierarchical.Node
+import org.junit.platform.engine.support.hierarchical.Node.DynamicTestExecutor
+import org.opentest4j.AssertionFailedError
+import org.pkl.lsp.ast.PklModuleImpl
+import org.pkl.lsp.ast.PklNode
+import org.pkl.lsp.ast.Terminal
+
+class ParserSnippetTestEngine : HierarchicalTestEngine<ParserSnippetTestEngine.ExecutionContext>() {
+
+  private val currentWorkingDir: Path
+    get() = Path.of(System.getProperty("user.dir"))
+
+  private val rootProjectDir: Path by lazy {
+    val workingDir = currentWorkingDir
+    workingDir.takeIf { it.resolve("settings.gradle.kts").exists() }
+      ?: workingDir.parent.takeIf { it.resolve("settings.gradle.kts").exists() }
+      ?: workingDir.parent.parent.takeIf { it.resolve("settings.gradle.kts").exists() }
+      ?: throw AssertionError("Failed to locate root project directory.")
+  }
+
+  private val testClass: KClass<*> = ParserSnippetTests::class
+
+  private val includedTests: List<Regex> = listOf(Regex(".*"))
+
+  @Suppress("RegExpUnexpectedAnchor") private val excludedTests: List<Regex> = listOf(Regex("$^"))
+
+  private val inputDir: Path = rootProjectDir.resolve("src/test/files/ParserSnippetTests/inputs")
+
+  private val outputDir: Path = inputDir.resolve("../outputs")
+
+  private val isInputFile: (Path) -> Boolean = { it.isRegularFile() && it.extension == "pkl" }
+
+  private fun expectedOutputFileFor(inputFile: Path): Path {
+    val relativePath = inputDir.relativize(inputFile)
+    return outputDir.resolve(relativePath.toString().dropLast(4) + ".txt")
+  }
+
+  private val project = Project(PklLSPServer(true))
+
+  private fun PklNode.render(): String {
+    return buildString {
+      doRender(this)
+      appendLine()
+    }
+  }
+
+  private fun PklNode.doRender(sb: StringBuilder, indent: String = "", isFirst: Boolean = true) {
+    if (this is Terminal) return
+    if (!isFirst) {
+      sb.append("\n")
+    }
+    sb.append(indent)
+    sb.append(this::class.simpleName)
+    for (child in this.children) {
+      child.doRender(sb, "$indent  ", false)
+    }
+  }
+
+  private fun generateOutputFor(inputFile: Path): Pair<Boolean, String> {
+    return try {
+      val text = inputFile.readText()
+      val tsNode = project.pklParser.parse(text, null)
+      val mod = PklModuleImpl(tsNode, project.virtualFileManager.getEphemeral(text))
+      //      val file = project.virtualFileManager.getEphemeral(text)
+      //      val parsed = file.getModule().get()!!
+      true to mod.render()
+    } catch (err: Throwable) {
+      false to err.stackTraceToString()
+    }
+  }
+
+  class ExecutionContext : EngineExecutionContext
+
+  override fun getId(): String = this::class.java.simpleName
+
+  init {
+    // Enforce consistent locale for tests to avoid inconsistent formatting.
+    Locale.setDefault(Locale.ROOT)
+  }
+
+  override fun discover(
+    discoveryRequest: EngineDiscoveryRequest,
+    uniqueId: UniqueId,
+  ): TestDescriptor {
+    val packageSelectors = discoveryRequest.getSelectorsByType(PackageSelector::class.java)
+    val classSelectors = discoveryRequest.getSelectorsByType(ClassSelector::class.java)
+    val methodSelectors = discoveryRequest.getSelectorsByType(MethodSelector::class.java)
+    val uniqueIdSelectors = discoveryRequest.getSelectorsByType(UniqueIdSelector::class.java)
+
+    val packageName = testClass.java.`package`.name
+    val className = testClass.java.name
+
+    if (
+      methodSelectors.isEmpty() &&
+        (packageSelectors.isEmpty() || packageSelectors.any { it.packageName == packageName }) &&
+        (classSelectors.isEmpty() || classSelectors.any { it.className == className })
+    ) {
+
+      val rootNode = object : InputDirNode(uniqueId, inputDir, ClassSource.from(testClass.java)) {}
+      return doDiscover(rootNode, uniqueIdSelectors)
+    }
+
+    // return empty descriptor w/o children
+    return EngineDescriptor(uniqueId, javaClass.simpleName)
+  }
+
+  private fun doDiscover(
+    dirNode: InputDirNode,
+    uniqueIdSelectors: List<UniqueIdSelector>,
+  ): TestDescriptor {
+    dirNode.inputDir.useDirectoryEntries { children ->
+      for (child in children) {
+        val testPath = child.toNormalizedPathString()
+        val testName = child.fileName.toString()
+        if (child.isRegularFile()) {
+          if (
+            isInputFile(child) &&
+              includedTests.any { it.matches(testPath) } &&
+              !excludedTests.any { it.matches(testPath) }
+          ) {
+            val childId = dirNode.uniqueId.append("inputFileNode", testName)
+            if (
+              uniqueIdSelectors.isEmpty() ||
+                uniqueIdSelectors.any { childId.hasPrefix(it.uniqueId) }
+            ) {
+              dirNode.addChild(InputFileNode(childId, child))
+            } // else skip
+          }
+        } else {
+          val childId = dirNode.uniqueId.append("inputDirNode", testName)
+          dirNode.addChild(
+            doDiscover(
+              InputDirNode(childId, child, DirectorySource.from(child.toFile())),
+              uniqueIdSelectors,
+            )
+          )
+        }
+      }
+    }
+    return dirNode
+  }
+
+  override fun createExecutionContext(request: ExecutionRequest) = ExecutionContext()
+
+  private open inner class InputDirNode(
+    uniqueId: UniqueId,
+    val inputDir: Path,
+    source: TestSource,
+  ) :
+    AbstractTestDescriptor(uniqueId, inputDir.fileName.toString(), source), Node<ExecutionContext> {
+    override fun getType() = Type.CONTAINER
+  }
+
+  private inner class InputFileNode(uniqueId: UniqueId, private val inputFile: Path) :
+    AbstractTestDescriptor(
+      uniqueId,
+      inputFile.fileName.toString(),
+      FileSource.from(inputFile.toFile()),
+    ),
+    Node<ExecutionContext> {
+
+    override fun getType() = Type.TEST
+
+    override fun execute(
+      context: ExecutionContext,
+      dynamicTestExecutor: DynamicTestExecutor,
+    ): ExecutionContext {
+
+      val (success, actualOutput) = generateOutputFor(inputFile)
+      val expectedOutputFile = expectedOutputFileFor(inputFile)
+
+      SnippetOutcome(expectedOutputFile, actualOutput, success).check()
+
+      return context
+    }
+  }
+
+  data class SnippetOutcome(val expectedOutFile: Path, val actual: String, val success: Boolean) {
+    private val expectedErrFile =
+      expectedOutFile.resolveSibling(expectedOutFile.toString().replaceAfterLast('.', "err"))
+
+    private val expectedOutExists = expectedOutFile.exists()
+    private val expectedErrExists = expectedErrFile.exists()
+    private val overwrite
+      get() = System.getenv().containsKey("OVERWRITE_SNIPPETS")
+
+    private val expected by lazy {
+      when {
+        expectedOutExists && expectedErrExists ->
+          fail("Test has both expected out and .err files: $displayName")
+        expectedOutExists -> expectedOutFile.readText()
+        expectedErrExists -> expectedErrFile.readText()
+        else -> ""
+      }
+    }
+
+    private val displayName by lazy {
+      val path = expectedOutFile.toString()
+      val baseDir = "src/test/files"
+      val index = path.indexOf(baseDir)
+      val endIndex = path.lastIndexOf('.')
+      if (index == -1 || endIndex == -1) path else path.substring(index + baseDir.length, endIndex)
+    }
+
+    fun check() {
+      when {
+        success && !expectedOutExists && !expectedErrExists && actual.isBlank() -> return
+        !success && expectedOutExists && !overwrite ->
+          failWithDiff("Test was expected to succeed, but failed: $displayName")
+        !success && expectedOutExists -> {
+          expectedOutFile.deleteExisting()
+          expectedErrFile.writeText(actual)
+          fail("Wrote file $expectedErrFile for $displayName and deleted $expectedOutFile")
+        }
+        success && expectedErrExists && !overwrite ->
+          failWithDiff("Test was expected to fail, but succeeded: $displayName")
+        success && expectedErrExists -> {
+          expectedErrFile.deleteExisting()
+          expectedOutFile.writeText(actual)
+          fail("Wrote file $expectedOutFile for $displayName and deleted $expectedErrFile")
+        }
+        !expectedOutExists && !expectedErrExists && actual.isNotBlank() -> {
+          val file = if (success) expectedOutFile else expectedErrFile
+          file.createParentDirectories().writeText(actual)
+          failWithDiff("Created missing file $file for $displayName")
+        }
+        else -> {
+          assert(success && expectedOutExists || !success && expectedErrExists)
+          if (actual != expected) {
+            if (overwrite) {
+              val file = if (success) expectedOutFile else expectedErrFile
+              file.writeText(actual)
+              fail("Overwrote file $file for $displayName")
+            } else {
+              failWithDiff("Output was different from expected: $displayName")
+            }
+          }
+        }
+      }
+    }
+
+    private fun failWithDiff(message: String): Nothing =
+      throw PklAssertionFailedError(message, expected, actual)
+  }
+
+  /**
+   * Makes up for the fact that [AssertionFailedError] doesn't print a diff, resulting in
+   * unintelligible errors outside IDEs (which show a diff dialog).
+   * https://github.com/ota4j-team/opentest4j/issues/59
+   */
+  class PklAssertionFailedError(message: String, expected: Any?, actual: Any?) :
+    AssertionFailedError(message, expected, actual) {
+    override fun toString(): String {
+      val patch =
+        DiffUtils.diff(expected.stringRepresentation.lines(), actual.stringRepresentation.lines())
+      return patch.deltas.joinToString("\n\n")
+    }
+  }
+
+  private fun Path.toNormalizedPathString(): String {
+    if (isWindows) {
+      return toString().replace("\\", "/")
+    }
+    return toString()
+  }
+}
--- src/test/kotlin/org/pkl/lsp/ParserTest.kt
+++ src/test/kotlin/org/pkl/lsp/ParserTest.kt
@@ -22,89 +22,8 @@ import org.junit.jupiter.api.Test
 import org.pkl.lsp.ast.*
 
 class ParserTest {
-
   private val project = Project(PklLSPServer(true))
   private val parser = project.pklParser
-
-  @Test
-  fun `parse types`() {
-    val code =
-      """
-        tunknown: unknown
-        
-        tnothing: nothing
-        
-        tmodule: module
-        
-        tstr: "foo"
-        
-        tqual: Mapping<Int, String>
-        
-        tpar: (String)
-        
-        tnull: Int?
-        
-        tconst: String(!isEmpty, !isBlank)
-        
-        tunion: Int|*String
-        
-        tfun: (Int, Int) -> String
-        """
-        .trimIndent()
-
-    val module = parse(code)
-    val rawProps = module.children
-
-    rawProps.forEach { assertThat(it).isInstanceOf(PklClassProperty::class.java) }
-    val props = rawProps.filterIsInstance<PklClassProperty>()
-
-    assertThat(props[0].type).isInstanceOf(PklUnknownType::class.java)
-
-    assertThat(props[1].type).isInstanceOf(PklNothingType::class.java)
-
-    assertThat(props[2].type).isInstanceOf(PklModuleType::class.java)
-
-    val type3 = props[3].type
-    assertThat(type3).isInstanceOf(PklStringLiteralType::class.java)
-    assertThat((type3 as PklStringLiteralType).stringConstant.value).isEqualTo("\"foo\"")
-
-    val type4 = props[4].type
-    assertThat(type4).isInstanceOf(PklDeclaredType::class.java)
-    val decl = type4 as PklDeclaredType
-    assertThat(decl.name.text).isEqualTo("Mapping")
-    assertThat(decl.typeArgumentList).isNotNull
-    assertThat(decl.typeArgumentList!!.types.size).isEqualTo(2)
-
-    val type5 = props[5].type
-    assertThat(type5).isInstanceOf(PklParenthesizedType::class.java)
-    assertThat((type5 as PklParenthesizedType).type.text).isEqualTo("String")
-
-    val type6 = props[6].type
-    assertThat(type6).isInstanceOf(PklNullableType::class.java)
-    assertThat((type6 as PklNullableType).type.text).isEqualTo("Int")
-
-    val type7 = props[7].type
-    assertThat(type7).isInstanceOf(PklConstrainedType::class.java)
-    val cons = type7 as PklConstrainedType
-    assertThat(cons.type?.text).isEqualTo("String")
-    assertThat(cons.exprs.size).isEqualTo(2)
-
-    val type8 = props[8].type
-    assertThat(type8).isInstanceOf(PklUnionType::class.java)
-    val union = type8 as PklUnionType
-    assertThat(union.leftType).isInstanceOf(PklDeclaredType::class.java)
-    assertThat(union.leftType.text).isEqualTo("Int")
-    assertThat(union.rightType).isInstanceOf(PklDefaultUnionType::class.java)
-    assertThat((union.rightType as PklDefaultUnionType).type.text).isEqualTo("String")
-
-    val type9 = props[9].type
-    assertThat(type9).isInstanceOf(PklFunctionType::class.java)
-    val func = type9 as PklFunctionType
-    assertThat(func.parameterList.size).isEqualTo(2)
-    assertThat(func.parameterList[0].text).isEqualTo("Int")
-    assertThat(func.parameterList[1].text).isEqualTo("Int")
-    assertThat(func.returnType.text).isEqualTo("String")
-  }
 
   @Test
   fun `Node text works with UTF-8 chars`() {
@@ -194,67 +113,6 @@ class ParserTest {
     assertThat(prop).isInstanceOf(PklClassProperty::class.java)
     prop as PklClassProperty
     assertThat(prop.parsedComment).isEqualTo("A doc\ncomment")
-  }
-
-  @Test
-  fun `when generators`() {
-    val code =
-      """
-      foo {
-        when (true) {
-          bar = 1
-        } else {
-          bar = 2
-        }
-      }
-    """
-        .trimIndent()
-    val mod = parse(code)
-    val prop = mod.children[0]
-    assertThat(prop).isInstanceOf(PklClassProperty::class.java)
-    prop as PklClassProperty
-    val whenGenerator = prop.objectBody!!.members.first() as PklWhenGenerator
-    assertThat(whenGenerator.thenBody).isNotNull
-    assertThat(whenGenerator.elseBody).isNotNull
-  }
-
-  @Test
-  fun `Nested comments inside binary expression are parsed correctly`() {
-    val code =
-      """
-      prop1 = one
-        // comment here
-        || two
-
-      prop2 = one /* comment */ && two
-      
-      prop3 = one
-        // comment
-        ?? two
-    """
-        .trimIndent()
-
-    val mod = parse(code)
-    val expr = assertClassPropertyExpr(mod.children[0])
-    assertThat(expr).isInstanceOf(PklLogicalOrExpr::class.java)
-    expr as PklLogicalOrExpr
-    assertThat(expr.leftExpr).isInstanceOf(PklUnqualifiedAccessExpr::class.java)
-    assertThat(expr.rightExpr).isInstanceOf(PklUnqualifiedAccessExpr::class.java)
-    assertThat(expr.operator.type).isEqualTo(TokenType.OR)
-
-    val expr2 = assertClassPropertyExpr(mod.children[1])
-    assertThat(expr2).isInstanceOf(PklLogicalAndExpr::class.java)
-    expr2 as PklLogicalAndExpr
-    assertThat(expr2.leftExpr).isInstanceOf(PklUnqualifiedAccessExpr::class.java)
-    assertThat(expr2.rightExpr).isInstanceOf(PklUnqualifiedAccessExpr::class.java)
-    assertThat(expr2.operator.type).isEqualTo(TokenType.AND)
-
-    val expr3 = assertClassPropertyExpr(mod.children[2])
-    assertThat(expr3).isInstanceOf(PklNullCoalesceExpr::class.java)
-    expr3 as PklNullCoalesceExpr
-    assertThat(expr3.leftExpr).isInstanceOf(PklUnqualifiedAccessExpr::class.java)
-    assertThat(expr3.rightExpr).isInstanceOf(PklUnqualifiedAccessExpr::class.java)
-    assertThat(expr3.operator.type).isEqualTo(TokenType.COALESCE)
   }
 
   @Test
