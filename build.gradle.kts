71d65300c282d5529d65452227e4851e97cf6569
Daniel Chao <daniel.h.chao@gmail.com>
2025-01-15 05:38:18 -0800

    Update copyright year and set up spotless (#58)
    This makes spotless update the copyright year when a file is touched.
    
    Also, update NOTICE.txt to include 2025.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 Apple Inc. and the Pkl project authors. All rights reserved.
+ * Copyright © 2024-2025 Apple Inc. and the Pkl project authors. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -386,7 +386,7 @@ sourceSets {
 private val licenseHeader =
   """
   /*
-   * Copyright © 2024 Apple Inc. and the Pkl project authors. All rights reserved.
+   * Copyright © ${'$'}YEAR Apple Inc. and the Pkl project authors. All rights reserved.
    *
    * Licensed under the Apache License, Version 2.0 (the "License");
    * you may not use this file except in compliance with the License.
@@ -403,7 +403,10 @@ private val licenseHeader =
 """
     .trimIndent()
 
+val originalRemoteName = System.getenv("PKL_ORIGINAL_REMOTE_NAME") ?: "origin"
+
 spotless {
+  ratchetFrom = "$originalRemoteName/main"
   kotlinGradle {
     ktfmt(libs.versions.ktfmt.get()).googleStyle()
     targetExclude("**/generated/**", "**/build/**")
