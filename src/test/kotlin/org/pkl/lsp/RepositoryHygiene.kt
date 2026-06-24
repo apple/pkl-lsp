249ae49943dccffe39754d8be9745ddded15b2c2
Dan Chao <dan.chao@apple.com>
2026-06-24 09:45:33 -0700

    Fix repository hygiene

--- src/test/kotlin/org/pkl/lsp/RepositoryHygiene.kt
+++ src/test/kotlin/org/pkl/lsp/RepositoryHygiene.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2025 Apple Inc. and the Pkl project authors. All rights reserved.
+ * Copyright © 2025-2026 Apple Inc. and the Pkl project authors. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,13 +57,12 @@ class RepositoryHygiene {
   }
 
   private fun checkOutputHasInput(inputs: Set<String>, output: String) {
-    val fileToCheck = outputRegex.replace(output, "$1.pkl")
+    val fileToCheck = output.replaceAfterLast('.', "pkl")
     assertThat(inputs).contains(fileToCheck)
   }
 
   companion object {
     private val inputRegex = Regex("(.*)\\.[^.]*(\\.pkl)")
-    private val outputRegex = Regex("(.*)\\.[^.]+\\.txt$")
 
     @JvmStatic
     fun getSnippetFolders() = buildList {
